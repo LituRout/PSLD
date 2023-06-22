5a02735cb8e80fa7e37d1fd5990757b9b6f96548
LituRout <litu.rout@utexas.edu>
2023-06-22 17:55:58 -0500

    add psld bip

--- stable-diffusion/run/inverse_bip.sh
+++ stable-diffusion/run/inverse_bip.sh
@@ -1,7 +1,9 @@
-export CUDA_VISIBLE_DEVICES='1'
+export CUDA_VISIBLE_DEVICES='2'
 python scripts/inverse.py \
-    --file_id='00014.png' \
+    --file_id='00003.png' \
     --task_config='configs/box_inpainting_config_psld.yaml' \
     --inpainting=1 \
     --general_inverse=0 \
-    --outdir='outputs/psld-samples-bip';
+    --gamma=1e-2 \
+    --omega=1e-1 \
+    --outdir='outputs/psld-samples-bip' 
